48b777daceae6f1c1361f1babcfd74df51242c7a
Manuel Patchou <manuel.mbankeu@tu-dortmund.de>
2025-06-16 16:21:28 +0200

    add radiodegradation control

---
+++ radiodegradation.py
@@ -0,0 +1,19 @@
+import requests
+import logging
+
+class VStingClient:
+    def __init__(self, host='kn-adrz-vsting.local', port=8000):
+        self.base_url = f"http://{host}:{port}"
+        self.logger = logging.getLogger(__name__)
+
+
+    def shape(self, latency_ms: int, jitter_ms: int = 0):
+        """Set Latency and jitter in ms"""
+        try:
+            response = requests.post(f"{self.base_url}/shape", data = {
+                "delay": latency_ms, "jitter": jitter_ms
+            })
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"Failed to send shape command: {e}")
